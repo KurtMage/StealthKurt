e76eaabe595200ba5d0c3994f022804b2d732418
Kurt <kmaegerle@gmail.com>
2014-10-05 08:25:29 -0700

    Chapter 1.08. Set up all the laser grids and did a high quality bake for the lighting.

---
+++ Assets/Scripts/AlarmSystems/LaserSwitchDeactivation.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LaserSwitchDeactivation : MonoBehaviour {
+
+	public GameObject laser;
+	public Material unlockedMat;
+
+	private GameObject player;
+
+	void Awake()
+	{
+		player = GameObject.FindGameObjectWithTag (Tags.player);
+	}
+
+	void OnTriggerStay(Collider other)
+	{
+		if (other.gameObject == player)
+		{
+			if (Input.GetButton("Switch"))
+			{
+				LaserDeactivation();
+			}
+		}
+	}
+
+	void LaserDeactivation()
+	{
+		laser.SetActive (false);
+
+		Renderer screen = transform.Find ("prop_switchUnit_screen").renderer;
+
+		screen.material = unlockedMat;
+		audio.Play ();
+	}
+}
