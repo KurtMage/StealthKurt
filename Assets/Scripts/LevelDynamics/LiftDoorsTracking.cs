91a654a1da8044a2415838034105e35e84f93bf9
Kurt <kmaegerle@gmail.com>
2014-10-05 13:22:20 -0700

    Chapter 3.05. Set up the Lift and the double doors on the lift.

--- Assets/Scripts/LevelDynamics/LiftDoorsTracking.cs
+++ Assets/Scripts/LevelDynamics/LiftDoorsTracking.cs
@@ -14,10 +14,10 @@ public class LiftDoorsTracking : MonoBehaviour {
 
 	void Awake()
 	{
-		leftOuterDoor = GameObject.Find ("door_exitOuter_left_001").transform;
-		rightOuterDoor = GameObject.Find ("door_exitOuter_right_001").transform;
-		leftInnerDoor = GameObject.Find ("door_exitInner_left_001").transform;
-		rightInnerDoor = GameObject.Find ("door_exitInner_right_001").transform;
+		leftOuterDoor = GameObject.Find ("door_exit_outer_left_001").transform;
+		rightOuterDoor = GameObject.Find ("door_exit_outer_right_001").transform;
+		leftInnerDoor = GameObject.Find ("door_exit_inner_left_001").transform;
+		rightInnerDoor = GameObject.Find ("door_exit_inner_right_001").transform;
 
 		leftClosedPositionX = leftInnerDoor.position.x;
 		rightClosedPositionX = rightInnerDoor.position.x;
