9df348d213d85cc5455b9d4a68c311576b4a2090
Kurt <kmaegerle@gmail.com>
2014-10-06 22:24:13 -0700

    Chapter 4.06. about 5 minutes in on the video. Set up some of the gun and script.

---
+++ Assets/EnemyShooting.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyShooting : MonoBehaviour {
+
+	public float maxDamage = 120f;
+	public float minDamage = 45f;
+	public AudioClip shotClip;
+	public float flashIntensity = 3f;
+	public float fadeSpeed = 10f;
+
+	private Animator anim;
+	private HashIDs hash;
+	private LineRenderer laserShotLine;
+	private Light laserShotLight;
+	private SphereCollider col;
+	private Transform player;
+	private PlayerHealth playerHealth;
+	private bool shooting;
+	private float scaledDamage;
+}
